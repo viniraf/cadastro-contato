3f86c1cdcac2a2d1c0f96bb1b26bf3199626a679
Vinicius Borges <vinicius.borges@flex.com>
2022-10-17 10:07:49 -0300

    Validacao campos na tela editar contato

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -64,8 +64,12 @@ namespace ControleDeContatos.Controllers
         [HttpPost]
         public IActionResult Alterar(ContatoModel contato) {
 
-            _contatoRepositorio.Atualizar(contato);
-            return RedirectToAction("Index");
+           if (ModelState.IsValid) {
+                _contatoRepositorio.Atualizar(contato);
+                return RedirectToAction("Index");
+            }
+
+            return View("Editar", contato);
         }
     }
 }
