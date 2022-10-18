a2803262a7a5625dedb8cf9bad1a41cd92f28332
Vinicius Borges <vinicius.borges@flex.com>
2022-10-18 09:57:14 -0300

    Importacao e configuracao datatable jQuery

--- ControleDeContatos/wwwroot/js/site.js
+++ ControleDeContatos/wwwroot/js/site.js
@@ -3,11 +3,39 @@
 
 // Write your JavaScript code.
 
-//$('.close-alert').click(function () {
-//    $('.alert').hide('hide');
-//});
+// Implementação para funcionar datatable traduzido e configurado
+$(document).ready(function () {
+    $('#table-contatos').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+});
 
-
+// Implementação para mensagem após ações crud sairem da tela automaticamente
 $('.alert').fadeIn().delay(1000).fadeOut(function () {
     $(this).remove()
 });
