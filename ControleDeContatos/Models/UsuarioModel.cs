bfbf8569719fd4342f78af51ee92644ad43e0cd6
Vinicius Borges <vinicius.borges@flex.com>
2022-10-19 14:56:36 -0300

    Criacao rascunho tela novo usuario

--- ControleDeContatos/Models/UsuarioModel.cs
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -1,6 +1,7 @@
 ﻿using ControleDeContatos.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,17 @@ namespace ControleDeContatos.Models {
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Digite o nome do usuário")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Digite o Login do usuário")]
         public string Login { get; set; }
 
+        [Required(ErrorMessage = "Digite o senha do usuário")]
         public string Senha { get; set; }
 
+        [Required(ErrorMessage = "Digite o E-mail do usuário")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido")]
         public string Email { get; set; }
 
         public PerfilEnum Perfil { get; set; }
